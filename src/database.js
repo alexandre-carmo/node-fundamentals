316ad2c5a5d037c7dea05351d78a19e4ef13d3f5
Alexandre Moraes <a.moraesdocarmo@gmail.com>
2023-02-07 07:44:46 -0300

    Add: search

--- src/database.js
+++ src/database.js
@@ -20,8 +20,16 @@ export class Database {
     fs.writeFile(databasePath, JSON.stringify(this.#database))
   }
 
-  select(table) {
+  select(table, search) {
     const data = this.#database[table] ?? []
+
+    if (search) {
+      data = data.filter(row => {
+        return Object.entries(search).some(([key, value]) => {
+          return row[key].includes(value)
+        })
+      })
+    }
 
     return data
 
